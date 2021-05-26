b1d164341a92c7f36663536b2b5957759d6b4fa6
Rob Marissen <50015273+524D@users.noreply.github.com>
2021-05-26 20:46:20 +0200

    Corrected and updated copyright message

--- mzrecal.go
+++ mzrecal.go
@@ -1,7 +1,5 @@
-// Copyright 2018,2019,2020 Rob Marissen.
-// All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
+// Copyright 2018 Rob Marissen.
+// SPDX-License-Identifier: MIT
 
 package main
 
