95c9c9da352c9047e9e2faba5ae94f9cb309d3e5
rjmarissen <r.j.marissen@lumc.nl>
2021-10-05 15:20:15 +0200

    Added flags so that builds are binary reproducable

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ VERSION=$(git describe --abbrev --dirty --always --tags)
 VERSION=${VERSION#"v"}
 
 echo 'Building mzrecal for Linux/amd64'
-GOOS=linux GOARCH=amd64 go build -a -ldflags "-extldflags \"-static\" -X main.progVersion=${VERSION}" -o $TARGET_DIR/mzrecal
+GOOS=linux GOARCH=amd64 go build -trimpath -a -ldflags "-extldflags \"-static\" -buildid= -X main.progVersion=${VERSION}" -o $TARGET_DIR/mzrecal
 
 echo 'Building mzrecal for Windows/amd64'
-GOOS=windows GOARCH=amd64 go build -a -ldflags "-extldflags \"-static\" -X main.progVersion=${VERSION}" -o $TARGET_DIR/mzrecal.exe
+GOOS=windows GOARCH=amd64 go build -trimpath -a -ldflags "-extldflags \"-static\" -buildid= -X main.progVersion=${VERSION}" -o $TARGET_DIR/mzrecal.exe
